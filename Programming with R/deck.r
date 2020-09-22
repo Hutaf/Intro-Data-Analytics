fe340daa8a1a5185333e7ede79cde137b253d22b
Hutaf Raja <hotaf4@gmail.com>
2020-09-22 10:20:53 +0300

    Add files via upload

---
+++ Programming with R/deck.r
@@ -0,0 +1,36 @@
+deck <- read.csv("/cloud/project/deck.csv")
+View(deck)
+
+# Blackjack Cards
+blackjack <- deck
+face<- c("king","queen", "jack")
+blackjack$value[blackjack$face %in% face]
+blackjack[blackjack$face %in% face,]
+blackjack$value[blackjack$face %in% face]<-10
+blackjack$value[blackjack$face == "ace"]<-NA
+View(blackjack)
+
+
+# hearts Cards
+hearts<- deck
+hearts$value <- 0
+hearts$value[hearts$suit == "hearts"]<- 1
+hearts$value[hearts$suit == "spades" & hearts$face == "queen"]<- 13
+View(hearts)
+
+# function for shuffling the deck
+shuffle <- function(cards){
+  cards[sample(1:52, size = 52),]}
+
+
+#function for dealing cards to 2 players in hearts
+Hearts_Deal <- function(cards){
+  cards[1,]
+  assign("Hearts", hearts[-1,], globalenv())}
+
+#function for dealing cards to 2 players and a dealer in blackjack
+Blackjack_Deal <- function(cards){
+  cards[1,]
+  assign("21Deal", blackjack[-1,], globalenv())}
+
+
